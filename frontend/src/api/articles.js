695dfad35fb08a9956a533ec42f81fd4e02cb685
2004khushi <goyalkhushi1004@gmail.com>
2025-12-25 00:36:43 +0530

    w

--- frontend/src/api/articles.js
+++ frontend/src/api/articles.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:8000/api/articles";
+const API = "http://127.0.0.1:8000/api/articles";
 
 export const getArticles = async () => {
   try {
